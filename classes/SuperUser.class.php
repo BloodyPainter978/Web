a0340cad38a68cc21d60f1072c7832c5d9fdef8b
Aza <65484188+BloodyPainter978@users.noreply.github.com>
2020-05-17 14:10:18 +0600

    Create SuperUser.class.php

---
+++ classes/SuperUser.class.php
@@ -0,0 +1,25 @@
+<?
+class SuperUser extends User{
+    public $role;
+    static public $countSuperUser;
+
+    function __construct($name,$login,$password,$role){
+        parent::__construct($name,$login,$password);
+        $this->role = $role;
+        ++self::$countSuperUser;
+        
+        echo "".self::$countSuperUser." класса '".__CLASS__."'.
+        role: '{$this->role}';
+        <br>\n";
+    }
+
+    function showInfo(){
+        echo "Объект класса '".__CLASS__."'.
+      name: '{$this->name}';
+      login: '{$this->login}';
+      password: '{$this->password}';
+      role: '{$this->role}';
+      <br>\n";
+    }
+}
+?>
